41e03813c119ac4a8ca85648465c6478497b7f32
ShanRB <shanrb119@gmail.com>
2020-03-26 20:27:22 +1100

    load file test run

---
+++ rank.py
@@ -0,0 +1,15 @@
+import json
+
+filename = "data/smallTwitter.json"
+i = 0
+file0 = open(filename,'r')
+
+i = 0
+for line in file0:
+    i = i + 1
+    if(line.endswith(',\n') and i <=5):
+        line = line[:-2]
+        data = json.loads(line)
+        print(f'>>>{i}')
+        for key,value in data['doc']['entities'].items():
+            print(f'{key}: {value}')
